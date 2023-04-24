dc18d616e56f9dca8f20dfb531b96048196106b1
Efren David <efren280892@gmail.com>
2023-04-23 23:03:29 -0300

    Casi terminando

--- practica.py
+++ practica.py
@@ -84,7 +84,7 @@ def definir_iniciales_nombre(heroes:dict,clave:str):
         heroes[clave]=extraer_iniciales(lista_personajes,heroes["nombre"])
         return True
 
-definir_iniciales_nombre(lista_personajes[0],"Iniciales")
+#definir_iniciales_nombre(lista_personajes[0],"Iniciales")
 
 
 
@@ -171,7 +171,6 @@ como máximo 10 caracteres (contando todos los caracteres, inclusive el
 guión). Se deberá completar con ceros para que todos queden del mismo
 largo
 
-
 Algunos ejemplos:
 F-00000001
 M-00000002
@@ -195,9 +194,10 @@ def generar_codigo_heroe(id_heroe:int,genero_heroe:str):
         return "N/A"
     elif len(str(id_heroe))>8:  # puede ser 7 tambien "NB"
         return "N/A"
+        
     else:
         if genero_heroe.upper()=="NB":
-            codigo=f"{genero_heroe.upper()}-{str(id_heroe).zfill(7)}" #consultar mejor forma
+            codigo=f"{genero_heroe.upper()}-{str(id_heroe).zfill(8)}" #consultar mejor forma
             codigo=codigo[:10]
             print(codigo)
             return codigo
@@ -206,9 +206,7 @@ def generar_codigo_heroe(id_heroe:int,genero_heroe:str):
             codigo=codigo[:10]
             print(codigo)
             return codigo
-
-    
-#generar_codigo_heroe(1234567,"NB")
+#generar_codigo_heroe(12345678,"NB")
 
 
 """
@@ -231,49 +229,391 @@ True, en caso de encontrarse un error retornar False
 
 """
 def agregar_codigo_heroe(heroe:dict,id_heroe:int)-> bool:
+
     if not heroe:
         print("diccionario vacio")
         return False
     
-    if generar_codigo_heroe(id_heroe,"M")=="N/A":
+    if generar_codigo_heroe(id_heroe,heroe["genero"])=="N/A":
         print("supera 10 caracteres")
         return False
     else:
-        heroe["codigo_heroe"]=generar_codigo_heroe(id_heroe,"NB") #Error debido a la funcion que necesita | funcion anterior
-        print(heroe)
+        heroe["codigo_heroe"]=generar_codigo_heroe(id_heroe,heroe["genero"]) #Error debido a la funcion que necesita | funcion anterior
+        
         return True
 
-agregar_codigo_heroe(lista_personajes[0],12345678)
+#agregar_codigo_heroe(lista_personajes[0],12345678)
 
 
 
+""" 2.3
+2.3. Crear la función ‘stark_generar_codigos_heroes’ la cual recibirá como
+parámetro:
+● lista_heroes: la lista de personajes
+La función deberá iterar la lista de personajes y agregarle el código a cada
+uno de los personajes.
+El código del héroe (id_heore) surge de la posición del mismo dentro de la
+lista_heroes (comenzando por el 1).
+Reutilizar la función agregar_codigo_heroe pasándole como argumentos el
+héroe que se está iterando y el id_heroe
+Una vez finalizado imprimir por pantalla un mensaje como el siguiente:
+(## representa la cantidad de códigos generados):
+Se asignaron ## códigos
+* El código del primer héroe es: M-00000001
+* El código del del último héroe es: M-00001224
+La función deberá validar::
+● La lista contenga al menos un elemento
+● Todos los elementos de la lista sean del tipo diccionario
 
-# 2.3. Crear la función ‘stark_generar_codigos_heroes’ la cual recibirá como
-# parámetro:
-# ● lista_heroes: la lista de personajes
-# La función deberá iterar la lista de personajes y agregarle el código a cada
-# uno de los personajes.
-# El código del héroe (id_heore) surge de la posición del mismo dentro de la
-# lista_heroes (comenzando por el 1).
-# Reutilizar la función agregar_codigo_heroe pasándole como argumentos el
-# héroe que se está iterando y el id_heroe
-# Una vez finalizado imprimir por pantalla un mensaje como el siguiente:
-# (## representa la cantidad de códigos generados):
-# Se asignaron ## códigos
-# * El código del primer héroe es: M-00000001
-# * El código del del último héroe es: M-00001224
-# La función deberá validar::
-# ● La lista contenga al menos un elemento
-# ● Todos los elementos de la lista sean del tipo diccionario
+● Todos los elementos contengan la clave ‘genero’
+En caso de encontrar algún error, informar por pantalla: ‘El origen de datos no
+contiene el formato correcto’
+La función no retorna ningún valor.
+"""
 
-# ● Todos los elementos contengan la clave ‘genero’
-# En caso de encontrar algún error, informar por pantalla: ‘El origen de datos no
-# contiene el formato correcto’
-# La función no retorna ningún valor.
+
 
 def stark_generar_codigos_heroes(lista:list):
+    cont_heroes=0
+    if type(lista) != list or len(lista) == 0:
+        print("Error no recibe ninguna lista")
+    
     for heroe in lista:
-        id_heroe=lista.index(heroe)+1
-        print(id_heroe)
         
-stark_generar_codigos_heroes(lista_personajes)
+        if type(heroe) != dict:
+            print("El origen de datos no contiene el formato correcto")
+        elif not "genero" in heroe:
+            print("El origen de datos no contiene el formato correcto genero no esta en dicc")
+        else:
+            cont_heroes+=1
+            id_heroe=lista.index(heroe)+1
+            agregar_codigo_heroe(heroe,id_heroe)
+            
+    primer_cod=lista[0]["codigo_heroe"]
+    ultimo_cod=lista[-1]["codigo_heroe"]
+    print(f"{cont_heroes} representa la cantidad de codigos generados \n Se asignaron {cont_heroes} codigos")
+    print(f"El codigo del primer heroe es {primer_cod} ultimo codigo {ultimo_cod} ")  # consultar si el codigo generado es el correcto el ultimo es 24 no 1224
+    
+#stark_generar_codigos_heroes(lista_personajes)
+
+"""
+3.1. Crear la función ‘sanitizar_entero’ la cual recibirá como parámetro:
+● numero_str: un string que representa un posible número entero
+La función deberá analizar el string recibido y determinar si es un número
+entero positivo. La función debe devolver distintos valores según el problema
+encontrado:
+● Si contiene carácteres no numéricos retornar -1 listo
+● Si el número es negativo se deberá retornar un -2 listo
+● Si ocurren otros errores que no permiten convertirlo a entero entonces
+se deberá retornar -3
+También deberá quitar los espacios en blanco de atras y adelante del string
+en caso que los tuviese
+En caso que se verifique que el texto contenido en el string es un número
+entero positivo, retornarlo convertido en entero
+"""
+
+def sanitizar_entero(numero_str:str)-> int:
+    
+    numero_str = numero_str.strip() 
+    
+    if numero_str.startswith("-") and numero_str[1:].isdigit():
+        return -2
+    if numero_str.isalpha():
+        return -1
+    if numero_str.isdigit():
+        numero=int(numero_str)
+        return numero
+    else:
+        return -3
+    
+##test=sanitizar_entero("58")
+#print(test)
+
+""" 3.2
+Crear la función ‘sanitizar_flotante’ la cual recibirá como parámetro:
+● numero_str: un string que representa un posible número decimal
+La función deberá analizar el string recibido y determinar si es un número
+flotante positivo. La función debe devolver distintos valores según el
+problema encontrado:
+
+● Si contiene carácteres no numéricos retornar -1
+● Si el número es negativo se deberá retornar un -2
+● Si ocurren otros errores que no permiten convertirlo a entero entonces
+se deberá retornar -3
+También deberá quitar los espacios en blanco de atras y adelante del string
+en caso que los tuviese
+En caso que se verifique que el texto contenido en el string es un número
+flotante positivo, retornarlo convertido en flotante
+"""
+
+def sanitizar_flotante(numero_str:str):
+    
+
+    numero=numero_str.strip()
+    
+    if  not numero.startswith("-")  and numero.replace(".","").isalpha():
+        return -1
+    
+    # Verificar si el número es negativo
+    if numero_str.startswith('-') and numero[1:].replace(".","").isnumeric():
+        return -2
+
+    try:
+        numero_float=float(numero)
+        return numero_float
+    except :
+        return -3
+
+#print(sanitizar_flotante("-5.50"))
+
+"""3.3.
+Crear la función ‘sanitizar_string’’ la cual recibirá como parámetro
+
+● valor_str: un string que representa el texto a validar
+● valor_por_defecto: un string que representa un valor por defecto
+(parámetro opcional, inicializarlo con ‘-’)
+
+La función deberá analizar el string recibido y determinar si es solo texto (sin
+números). En caso de encontrarse números retornar “N/A”
+En el caso que valor_str contenga una barra ‘/’ deberá ser reemplazada por un
+espacio
+El espacio es un caracter válido
+En caso que se verifique que el parámetro recibido es solo texto, se deberá
+retornar el mismo convertido todo a minúsculas
+En el caso que el texto a validar se encuentre vacío y que nos hayan pasado
+un valor por defecto, entonces retornar el valor por defecto convertido a
+minúsculas
+Quitar los espacios en blanco de atras y adelante de ambos parámetros en
+caso que los tuviese
+"""
+def sanitizar_string(valor_str:str,valor_por_defecto:str="-"):
+    
+    valor_s=valor_str.strip()
+    valor_p_d=valor_por_defecto.strip()
+    
+    if not valor_str and valor_por_defecto != '-':
+        return valor_p_d.lower()
+    
+    valor=valor_s.replace("/"," ")
+
+    if valor_s.replace("/","").isalpha():
+        return valor.lower()
+    else:
+        return "N/A"
+
+#print(sanitizar_string("  h/o/l/@"))
+
+
+
+""" 3.4
+3.4. Crear la función ‘sanitizar_dato’ la cual recibirá como parámetros:
+● heroe: un diccionario con los datos del personaje
+● clave: un string que representa el dato a sanitizar (la clave del
+diccionario). Por ejemplo altura
+
+● tipo_dato: un string que representa el tipo de dato a sanitizar. Puede
+tomar los valores: ‘string’, ‘entero’ y ‘flotante’
+La función deberá sanitizar el valor del diccionario correspondiente a la clave
+y al tipo de dato recibido
+Para sanitizar los valores se deberán utilizar las funciones creadas en los
+puntos 3.1, 3.2, 3.3 y 3.4
+
+Se deberá validar:
+● Que tipo_dato se encuentre entre los valores esperados (‘string, ‘entero,
+‘flotante)’ la validación debe soportar que nos lleguen mayúsculas o
+minúsculas. En caso de encontrarse un valor no válido informar por
+pantalla: ‘Tipo de dato no reconocido’
+
+● Que clave exista como clave dentro del diccionario heroe. En caso de
+no encontrarse, informar por pantalla: ‘La clave especificada no
+existe en el héroe’. (en este caso la validación es sensible a
+mayúsculas o minúsculas)
+Ejemplo de llamada a la función válida:
+sanitizar_dato(dict_personaje, “altura”, “Flotante”)
+La función deberá devolver True en caso de haber sanitizado algún dato y
+False en caso contrario."""
+
+def sanitizar_dato(heroe:dict,clave:str,tipo_de_dato:str):
+    
+    clave_str=clave.lower()
+    tipo_d_d=tipo_de_dato.lower()
+    
+    if  clave_str not in heroe:
+        print("La clave especificada no existe en el heroe")
+        return False
+    if tipo_d_d not in ["flotante","entero","string"]:
+        print("Tipo de dato no reconocido")
+        return False
+
+    
+    match (tipo_d_d):   # es necesario validar el tipo de la clave con el typo del dato
+        case "flotante":
+            sanitizar_flotante(heroe[clave_str])
+            #print(sanitizar_flotante(heroe[clave_str]))
+            
+            return True
+
+        case "string":
+            sanitizar_string(heroe[clave_str])
+            #print(sanitizar_string(heroe[clave_str]))
+            
+            return True
+        
+        case "entero":
+            sanitizar_entero(heroe[clave_str])
+            #print(sanitizar_entero(heroe[clave_str]))
+            
+            return True
+        
+        case _:
+            return False
+
+#print(sanitizar_dato(lista_personajes[0],"nombre","flotante"))
+
+"""
+3.5. Crear la función stark_normalizar_datos’ la cual recibirá como
+parámetros:
+● lista_heroes: la listas personajes
+La función deberá recorrer la lista de héroes y sanitizar los valores solo de las
+siguientes claves: ‘altura’, ‘peso’, ‘color_ojos’, ‘color_pelo’, ‘fuerza’ e
+‘inteligencia’
+● Un vez finalizado el proceso mostrar el mensaje ‘Datos normalizados’,
+● Validar que la lista de héroes no esté vacía para realizar sus acciones,
+caso contrario imprimirá el mensaje: “Error: Lista de héroes vacía”
+● La función no retorna nada
+● Reutilizar la función sanitizar_dato
+
+"""
+
+def stark_normalizar_datos(lista:list):
+    if len(lista)==0:
+        print("Error: Lista de héroes vacía")
+    
+    for heroe in lista:
+         for clave, valor in heroe.items():
+            if clave in ['altura', 'peso']:
+                sanitizar_dato(heroe,clave,"flotante")
+                print("ejecuto1")
+            if clave in ['color_ojos','color_pelo','inteligencia']:
+                sanitizar_dato(heroe,clave,"string")
+                print("ejecuto2")
+            if clave in ['fuerza']:
+                sanitizar_dato(heroe,clave,"entero")
+                print("ejecuto3")
+            print(lista)
+    
+#stark_normalizar_datos(lista_personajes)
+
+
+"""4.1
+Crear la función ‘generar_indice_nombres’ la cual recibirá como
+parámetro:
+● lista_heroes: la lista de personajes
+La función deberá iterar la lista de personajes y generar una lista donde cada
+elemento es cada palabra que componen el nombre de los personajes.
+Por ejemplo la lista que se deberá retornar tiene la siguiente forma:
+[‘Howard’, ‘the’, ‘Duck’, ‘Rocket’, ‘Raccoon’, ‘Wolverine’, … ]
+La función deberá validar que:
+● La lista contenga al menos un elemento
+● Todos los elementos de lista_heroes sean del tipo diccionario
+● Todos los elementos contengan la clave ‘nombre’
+
+En caso de encontrar algún error, informar por pantalla: ‘El origen de datos no
+contiene el formato correcto’
+"""
+
+def generar_indice_nombres(lista:list):
+    
+    if type(lista) !=list:
+        print("El origen de datos contiene un formato incorrecto")
+    
+    if len(lista)==0:
+        print("Error: Lista de héroes vacía")
+    
+    lista2=[]
+    for heroe in lista:
+        if type(heroe) != dict:
+            print("Error se encontro un elemento de tipo no diccionario" )
+        elif "nombre" not in heroe:
+            print("Hay elementos sin clave nombre")
+            break
+        else:
+            lista2 +=heroe["nombre"].split()
+    return lista2
+            
+            
+#generar_indice_nombres(lista_personajes)
+"""4.2
+4.2. Crear la función ‘stark_imprimir_indice_nombre’ la cual recibirá como
+parámetro:
+● lista_heroes: la lista de personajes
+La función deberá mostrar por pantalla el índice generado por la función
+generar_indice_nombres con todos los nombres separados con un guión.
+Por ejemplo:
+Howard-the-duck-Rocket-Raccoon-Wolverine…
+
+"""
+def stark_imprimir_indice_nombre(lista:list):
+    imprimir_lista=""
+    
+    for elemento in generar_indice_nombres(lista):
+        imprimir_lista +=f"{elemento}-"
+    
+    print(imprimir_lista)
+    
+#stark_imprimir_indice_nombre(lista_personajes)
+
+"""5.1
+5.1. Crear la función ‘convertir_cm_a_mtrs’ la cual recibirá como parámetro:
+● valor_cm: Un número que representa una medida en centímetros
+La función deberá retornar el número recibido, pero convertido a la unidad
+metros. La función deberá validar que el número recibido sea un número
+flotante positivo, en caso de no serlo retornar -1
+"""
+
+def convertir_cm_a_mtrs(valor_cm:int):
+    
+    if type(valor_cm) != float:
+        print("es otra cosa")
+        
+        return -1
+    
+    if type(valor_cm)== float and valor_cm > 0:
+        conversion=valor_cm/100
+        
+        return conversion
+    
+    elif type(valor_cm)== float and valor_cm < 0:
+        return -1
+    
+    else:
+        return -1
+
+#convertir_cm_a_mtrs()
+
+"""5.2
+5.2. Crear la función ‘generar_separador’ la cual recibirá como parámetro
+● patron: un carácter que se utilizará como patrón para generar el
+separador
+● largo: un número que representa la cantidad de caracteres que va
+ocupar el separador.
+● imprimir: un parámetro opcional del tipo booleano (por default definir
+en True)
+
+La función deberá generar un string que contenga el patrón especificado
+repitiendo tantas veces como la cantidad recibida como parámetro (uno junto
+al otro, sin salto de línea)
+Si el parámetro booleano recibido se encuentra en False se deberá solo
+retornar el separador generado. Si se encuentra en True antes de retornarlo,
+imprimirlo por pantalla
+La función deberá validar:
+● Que el parámetro patrón tenga al menos un carácter y como máximo
+dos
+● Que el parámetro largo sea un entero entre 1 y 235 inclusive
+En caso de no verificarse las validaciones devolver ‘N/A’
+Ejemplo de llamada:
+generar_separador(‘*’, 10)
+Ejemplo de salida:
+**********
+
+"""
